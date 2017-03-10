5ce42cbcb71000593c69dbf904a38908999c275d
clbokea <clbo@kea.dk>
2017-03-10 09:59:43 +0100

    øvelse

--- Code/Input.java
+++ Code/Input.java
@@ -1,13 +1,22 @@
 
 
 
-
 public class Input
 {
     public static void main(String[] args)
     {
-        System.out.print("Enter something:");
+        System.out.print("Hvad kunne du tænke dig (is eller sodavand)?:");
         String input = System.console().readLine();
-        System.out.println("You wrote: " + input);
+        
+        if(input.equals("is")) // input == "is"
+        {
+            System.out.println("You wrote: " + input);
+        } else if (input.equals("sodavand")) 
+        {
+            System.out.println("You wrote: " + input);
+        } else {
+            System.out.println("Else");
+        }
+        
     }
 }
